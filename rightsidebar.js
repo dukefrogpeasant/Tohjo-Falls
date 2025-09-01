2007535fcffcd798264f5652176aeec730ee3a41
dukefrogpeasant <yaxinjp@gmail.com>
2025-09-01 14:43:57 +0200

    DynamicPrepare
    Preparations for dynamic.

---
+++ rightsidebar.js
@@ -0,0 +1,90 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const sidebar = document.getElementById("right-sidebar");
+    if (!sidebar) return;
+    sidebar.innerHTML = `
+    <center>
+        <img src="./Visual/navbar/news.png"><br>
+        <table width="139"><tr>
+        <td width="139" style="border-left: 1 solid #000000; border-bottom: 1 solid #000000; border-right: 1 solid #000000; border-top: 1 solid #000000;" background="/web/20050207184020im_/http://tohjo.mybesthost.com/mbp.png">
+        <font face="verdana" size="2">
+            <div id="news-updates-section">
+                <marquee behavior="scroll" direction="up" scrollamount="2" style="height: 100px;">
+                    <a href="main.html">News & Updates</a><br>
+                    Welcome to Tohjo Falls!
+                </marquee>
+            </div>
+        </font>
+        </td></tr></table>
+        <img src="./Visual/navbar/affiliates.PNG"><br>
+        <table width="139"><tr>
+        <td id="affiliates-section" width="139" style="border-left: 1 solid #000000; border-bottom: 1 solid #000000; border-right: 1 solid #000000; border-top: 1 solid #000000;" background="/web/20050207184020im_/http://tohjo.mybesthost.com/mbp.png">
+            <a href="https://tohjo-falls.neocities.org/">
+                <img src="./Visual/header/tfaffiliate.png" alt="The Tohjo Falls">
+            </a><br>
+            <a href="http://angelfire.com/anime4/animefanime">
+                <img src="./Visual/header/animefanime.gif" border="0">
+            </a>
+            <a href="https://angelfire.com/pokemon2/pkmnjourney/linktous.html">
+                <img src="./Visual/header/pokejourneyani.gif" border="0">
+            </a>
+        </td>
+        </tr></table>
+        <img src="./Visual/navbar/tagboard.PNG"><br>
+        <table width="139"><tr>
+        <td width="139" style="border-left: 1 solid #000000; border-bottom: 1 solid #000000; border-right: 1 solid #000000; border-top: 1 solid #000000;" background="/web/20050207184020im_/http://tohjo.mybesthost.com/mbp.png">
+        <center>
+            <script language="javascript" src="https://web.archive.org/web/20050207184020js_/http://www.saybox.co.uk/output.php?user=pokestorm"></script>
+        </center>
+        </td></tr></table>
+        <img src="./Visual/navbar/topsites.PNG"><br>
+        <table width="139"><tr>
+        <td width="139" style="border-left: 1 solid #000000; border-bottom: 1 solid #000000; border-right: 1 solid #000000; border-top: 1 solid #000000;" background="/web/20050207184020im_/http://tohjo.mybesthost.com/mbp.png">
+        <font face="verdana" size="2">
+            &nbsp;<a href="index.html">Home</a><br>
+            &nbsp;<a href="index.html">Home</a><br>
+            &nbsp;<a href="index.html">Home</a><br>
+            &nbsp;<a href="index.html">Home</a><br>
+            &nbsp;<a href="index.html">Home</a><br>
+            &nbsp;<a href="index.html">Home</a><br>
+            &nbsp;<a href="index.html">Home</a><br>
+            &nbsp;<a href="index.html">Home</a><br>
+            &nbsp;<a href="index.html">Home</a><br>
+            &nbsp;<a href="index.html">Home</a><br>
+        </font>
+        </td></tr></table>
+        <img src="./Visual/navbar/networks.PNG"><br>
+        <table width="139"><tr>
+        <td width="139" style="border-left: 1 solid #000000; border-bottom: 1 solid #000000; border-right: 1 solid #000000; border-top: 1 solid #000000;" background="/web/20050207184020im_/http://tohjo.mybesthost.com/mbp.png">
+        <font face="verdana" size="2">
+            &nbsp;<a href="index.html">Home</a><br>
+            &nbsp;<a href="index.html">Home</a><br>
+            &nbsp;<a href="index.html">Home</a><br>
+            &nbsp;<a href="index.html">Home</a><br>
+            &nbsp;<a href="index.html">Home</a><br>
+            &nbsp;<a href="index.html">Home</a><br>
+            &nbsp;<a href="index.html">Home</a><br>
+            &nbsp;<a href="index.html">Home</a><br>
+            &nbsp;<a href="index.html">Home</a><br>
+            &nbsp;<a href="index.html">Home</a><br>
+        </font>
+        </td></tr></table>
+        <img src="./Visual/navbar/stats.PNG"><br>
+        <table width="139"><tr>
+        <td width="139" style="border-left: 1 solid #000000; border-bottom: 1 solid #000000; border-right: 1 solid #000000; border-top: 1 solid #000000;" background="/web/20050207184020im_/http://tohjo.mybesthost.com/mbp.png">
+        <font face="verdana" size="2">
+            &nbsp;<a href="index.html">Home</a><br>
+            &nbsp;<a href="index.html">Home</a><br>
+            &nbsp;<a href="index.html">Home</a><br>
+            &nbsp;<a href="index.html">Home</a><br>
+            &nbsp;<a href="index.html">Home</a><br>
+            &nbsp;<a href="index.html">Home</a><br>
+            &nbsp;<a href="index.html">Home</a><br>
+            &nbsp;<a href="index.html">Home</a><br>
+            &nbsp;<a href="index.html">Home</a><br>
+            &nbsp;<a href="index.html">Home</a><br>
+        </font>
+        </td></tr></table>
+        <br>
+    </center>
+    `;
+});
